1323985c599d2f837ec69e7fe5fa347fe48ecd2f
Janos Schwellach <janossch@amazon.com>
2020-05-21 09:29:14 +0800

    fixed the script

--- ipywidget.sh
+++ ipywidget.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-PACKAGES=("scikit-image" "ipympl")
-CONDA_PACKAGES=("nodejs" "ipympl" "ipywidgets")
-LAB_EXTENSIONS=("@jupyterlab/git" "@jupyter-widgets/jupyterlab-manager")
-ENVIRONMENT=mxnet_p36
 
 echo "installing conda packages in all environments"
+sudo -u ec2-user -i <<'EOF'
+CONDA_PACKAGES=("nodejs" "ipympl" "ipywidgets")
 for p in "${CONDA_PACKAGES[@]}"
 do
     echo "installing in base environment..."
@@ -20,8 +18,12 @@ do
         conda install "$p" --name "$env_name" --yes
     done
 done
-echo "installing conda packages done..."
+EOF
+echo "installing conda packages in all environments"
+
 echo "installing lab extensions"
+sudo -u ec2-user -i <<'EOF'
+LAB_EXTENSIONS=("@jupyterlab/git" "@jupyter-widgets/jupyterlab-manager")
 for e in "${LAB_EXTENSIONS[@]}"
 do
     echo "installing $e"
@@ -29,8 +31,13 @@ do
     jupyter labextension install "$e"
     source /home/ec2-user/anaconda3/bin/deactivate
 done
+EOF
 echo "installing lab extensions done"
+
 echo "installing python pip packages"
+sudo -u ec2-user -i <<'EOF'
+PACKAGES=("scikit-image" "ipympl")
+ENVIRONMENT=mxnet_p36
 source /home/ec2-user/anaconda3/bin/activate "$ENVIRONMENT"
 pip install --upgrade pip
 for p in "${PACKAGES[@]}"
@@ -38,3 +45,5 @@ do
     pip install --upgrade "$i"
 done
 source /home/ec2-user/anaconda3/bin/deactivate
+EOF
+echo "installing python pip packages done"
