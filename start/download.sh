29f7df5f6793bc86951514daf055290c2e6c0126
Janos Schwellach <janossch@amazon.com>
2020-05-22 10:30:28 +0800

    donwload script header

--- start/download.sh
+++ start/download.sh
@@ -4,7 +4,7 @@ set -e
 
 # Install example dataset from Amazon for training and CV workshop
 echo "downloading example dataset"
-sudo -u ec2-user -i << EOF
+sudo -u ec2-user -i << 'EOF'
 # Download dataset (if removed from tmp folder)
 BASEURL="https://d8mrh1kj01ho9.cloudfront.net/workshop/cv1/data/"
 FILES='example_dataset.pkl training_data.pkl test_data.pkl sample_model_output.csv'
